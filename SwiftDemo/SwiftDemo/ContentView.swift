2a1c8a8849109bac8959d0eb80751db55270eebf
ByungWooks <dlquddnr414@naver.com>
2022-11-21 21:22:51 +0900

    예제 파일 생성

---
+++ SwiftDemo/SwiftDemo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftDemo
+//
+//  Created by 병욱 on 2022/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftDemo/SwiftDemo/SwiftDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDemoApp.swift
+//  SwiftDemo
+//
+//  Created by 병욱 on 2022/11/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
